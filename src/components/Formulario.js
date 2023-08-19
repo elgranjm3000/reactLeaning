4087beb6b8fb7c0dbd2d63deaa52bba84e660a73
Joseph Muentes <elgranjm3000@gmail.com>
2023-08-18 20:44:07 -0400

    formularios send

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -9,18 +9,54 @@ class Formulario extends Component {
     generoHombreRef = React.createRef();
     generoMujerRef = React.createRef();
     generoOtroRef = React.createRef();
-    recibirFormulario = (e) => {
-        e.preventDefault();
-        console.log(this.nombreRef.current.value)
+
+    state = {
+        user: {}
     }
 
+    recibirFormulario = (e) => {
+
+        var genero = "Hombre"
+        if(this.generoHombreRef.current.checked){
+            genero = this.generoHombreRef.current.value
+        }else if(this.generoMujerRef.current.checked){
+            genero = this.generoMujerRef.current.value
+        }else{
+            genero = this.generoOtroRef.current.value
+        }
+        e.preventDefault();
+        var user = {
+            nombre:this.nombreRef.current.value,
+            apellido:this.apellidoRef.current.value,
+            bio:this.bioRef.current.value,
+            genero:genero
+        }
+        this.setState({
+            user:user
+        })
+    }
+
+  
+
     render() {
+        if(this.state.user.nombre){
+            var user = this.state.user;
+        }
 
         return (
             <div id="formulario">
                 <h1 className="subheader">Formulario</h1>
 
                 <div className="center" id="content">
+
+                    {this.state.user.nombre &&
+                        <div id="userData">
+                            <p>Nombre: <strong>{user.nombre}</strong> </p>
+                            <p>Apellido: <strong>{user.apellido}</strong> </p>
+                            <p>Biografia: <strong>{user.bio}</strong> </p>
+                            <p>Genero: <strong>{user.genero}</strong> </p>
+                        </div>
+                    }
 
                     <form className="mid-form" onSubmit={this.recibirFormulario}>
                         <div className="form-group">
